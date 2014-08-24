52b73276f707ffd34f9d2df763af8165431ecd37
Ulan <udegenbaev@gmail.com>
2014-08-24 10:46:16 +0200

    Test problem 01.

--- Problem01.hs
+++ Problem01.hs
@@ -1,11 +1,17 @@
 module Main where
 import Control.Applicative
 import Data.List
+import Test.QuickCheck
 
 -- The Smallest Free Number.
--- Given a list of integers without duplicates, find
+-- Given a list of natural numbers without duplicates, find
 -- the smallest natural number that is not in the list.
 
+-- Specification from the book:
+spec :: [Int] -> Int
+spec = head . ([0..] \\)
+
+-- The actual solution.
 solve :: [Int] -> Int
 solve xs = go 0 (length xs) xs
   where
@@ -16,6 +22,31 @@ solve xs = go 0 (length xs) xs
         (left, right) = partition (< middle) xs
         middle = (lower + higher + 1) `div` 2
 
+-- Tests
+newtype NatSetList = NatSetList [Int]
+    deriving (Eq, Ord, Show)
+
+deleteAny :: Eq a => [a] -> Gen (a, [a])
+deleteAny xs = do
+    x <- elements xs
+    return (x, delete x xs)
+
+permute :: Eq a => [a] -> Gen [a]
+permute [] = return []
+permute xs = do
+    (y, ys) <- deleteAny xs
+    ys' <- permute ys
+    return (y : ys')
+
+instance Arbitrary NatSetList where
+    arbitrary = sized $ \s -> do
+        n <- choose (1, max 1 s)
+        (_, xs) <- deleteAny [0 .. n]
+        xs' <- permute xs
+        return $ NatSetList xs'
+
+prop_spec = verboseCheck (\(NatSetList xs) -> spec xs == solve xs)
+
 main = do
     input <- (map read <$> words <$> getLine) :: IO [Int]
-    print . solve . filter (>= 0) $ input
+    print . solve $ input
